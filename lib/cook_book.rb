dc9197bfb67c3f73804ae0e4eaae82e1a08e1b7a
Sage Lee <sagemcklee@gmail.com>
2020-04-22 11:12:41 -0600

    Add attempts at iteration 4

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,7 +1,9 @@
+require 'date'
 class CookBook
-    attr_reader :recipes
+    attr_reader :recipes, :date
   def initialize
     @recipes = []
+    @date = Date.today.strftime("%m-%d-%Y")
   end
 
   def add_recipe(recipe)
@@ -21,7 +23,26 @@ class CookBook
   def highest_calorie_meal
     @recipes.max_by do |recipe|
       recipe.total_calories
-    end 
+    end
   end
 
+  def summary_helper_hash(recipe)
+    helper_hash = Hash.new
+    helper_inner_hash = Hash.new
+    recipe.ingredients_required.each do |ingredient, amount|
+      helper_inner_hash[:ingredient] = ingredient.name
+      helper_inner_hash[:amount] = amount
+    end
+    helper_hash[:ingredients] = helper_inner_hash
+    helper_hash
+  end
+
+  def summary
+    @recipes.map do |recipe|
+      outer_hash = Hash.new
+      outer_hash[:name] = recipe.name
+      outer_hash[:details] = summary_helper_hash(recipe)
+      outer_hash
+    end
+  end
 end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -12,7 +12,7 @@ class CookBookClass < Minitest::Test
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
     @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
-    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
@@ -54,4 +54,24 @@ class CookBookClass < Minitest::Test
     assert_equal @recipe2, @cookbook.highest_calorie_meal
   end
 
+  def test_it_can_list_the_date_the_cook_book_was_created
+    assert_equal "04-22-2020", @cookbook.date
+  end
+
+  def test_it_can_summarize
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 100)
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    expected =
+    [{:name=>"Mac and Cheese", :details=>
+      {:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"},
+      {:ingredient=>"Cheese", :amount=>"2 C"}], :total_calories=>440}},
+     {:name=>"Burger", :details=>
+      {:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"},
+      {:ingredient=>"Bun", :amount=>"100 g"}], :total_calories=>500}}]
+      assert_equal expected, @cookbook.summary
+  end
 end
